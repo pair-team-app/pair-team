3af2383af19d6be349846ac458fff04315b01bfb
mtholcombe <matt@designengine.ai>
2019-10-12 11:31:48 -0700

    common node processor

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@
 'use strict';
 
 
-// import chalk from 'chalk';
 // import stringify from 'json-stringify-safe';
 import projectName from 'project-name';
 import puppeteer from 'puppeteer';
@@ -30,24 +29,22 @@ export async function puppetWorker(url, playgroundID) {
 		}
 	];
 
-	const objs = Promise.all(await devices.map(async(device)=> {
+	const objs = await Promise.all(devices.map(async(device)=> {
 		const browser = await puppeteer.launch({
 			headless : true
 		});
 
 		const page = await browser.newPage();
 		await page.emulate(device);
-// 	await page.waitForSelector('[class="app"]');
-
 		await page.goto(url, { waitUntil : 'networkidle2' });
-// 		await page.content();
 
 		await consts(page);
 		await listeners(page);
 		await funcs(page);
 
+
 		const elements = await extractElements(page);
-		console.log(device.name, elements.buttons.map((button)=> ({ ...button,
+		console.log(device.name, elements.links.map((el)=> ({ ...el,
 			styles : {}
 		})));
 
@@ -61,17 +58,18 @@ export async function puppetWorker(url, playgroundID) {
 			links       : elements.links.map((link)=> (link.meta.href)).join(' ')
 		};
 
-// 		console.log('::::', extract.doc.styles);
-// 		console.log('::::', extract);
-// 		console.log('IMAGES -->', extract.elements.images[0]);
-// 		console.log('BUTTONS -->', extract.elements.buttons[0]);
-// 		console.log('BUTTONS -->', JSON.stringify([{...extract.elements.buttons[0], handle : null }, {...extract.elements.buttons[1], handle : null }], null, 2));
-// 		console.log('BUTTONS -->', JSON.stringify(extract.elements.buttons[0], null, 2));
-// 		console.log('BUTTONS -->', extract.elements.buttons[0].styles, Object.keys(extract.elements.buttons[0].styles).length);
-// 		console.log('LINKS -->',  [extract.elements.links[0].styles, extract.elements.links[1].styles]);
-// 		console.log('LINKS -->',  [extract.elements.links[0].styles.length]);
-// 		console.log('IMAGES -->', Object.keys(extract.elements.images[0].styles).length);
-// 	console.log('IMAGE[0].border -->', extract.elements.images[0].styles);
+// 		console.log('::::', doc);
+// 		console.log('::::', elements);
+// 		console.log('IMAGES -->', elements.images[0]);
+// 		console.log('BUTTONS -->', elements.buttons[0]);
+// 		console.log('BUTTONS -->', JSON.stringify(elements.buttons[0], (key, val)=> { console.log(key, ':', val, '\n- - - -')}, 2));
+// 		console.log('BUTTONS -->', JSON.stringify([{ ...elements.buttons[0], handle : null }, {... elements.buttons[1], handle : null }], null, 2));
+// 		console.log('BUTTONS -->', JSON.stringify(elements.buttons[0], null, 2));
+// 		console.log('BUTTONS -->', elements.buttons[0].styles, Object.keys(elements.buttons[0].styles).length);
+// 		console.log('LINKS -->',  [elements.links[0].styles, elements.links[1].styles]);
+// 		console.log('LINKS -->',  [elements.links[0].styles.length]);
+// 		console.log('IMAGES -->', Object.keys(elements.images[0].styles).length);
+// 	console.log('IMAGE[0].border -->', elements.images[0].styles);
 
 		await browser.close();
 
@@ -81,77 +79,6 @@ export async function puppetWorker(url, playgroundID) {
 	}));
 
 	return (objs);
-
-
-// 	const browser = await puppeteer.launch({
-// 		headless : true
-// 	});
-// 	const page = await browser.newPage();
-//
-// 	await page.setUserAgent('Mozilla/5.0 (iPhone; CPU iPhone OS 12_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13 Mobile/15E148 Safari/604.1');
-// 	await page.setViewport({
-// 		width    : 1125,
-// 		height   : 2436,
-// 		isMobile : true,
-// 		hasTouch : true,
-// 		deviceScaleFactor : 3
-// 	});
-//
-// // 	await page.waitForSelector('[class="app"]');
-//
-// 	await page.goto(url);
-// 	await page.content();
-//
-// 	await consts(page);
-// 	await listeners(page);
-// 	await funcs(page);
-//
-// 	const extract = {
-// 		html       : {
-// 			doc    : await page.content(),
-// 			styles : await page.evaluate(()=> (getComputedStyle(document.documentElement)))
-// 		},
-// 		elements   : await extractElements(page),
-// 		playground : null
-// 	};
-//
-// //		console.log('::::', extract.elements);
-// // 		console.log('IMAGES -->', extract.elements.images[0]);
-// // 		console.log('BUTTONS -->', extract.elements.buttons[0].styles);
-// // 		console.log('BUTTONS -->', JSON.stringify(extract.elements.buttons[0], null, 2));
-// 		console.log('BUTTONS -->', extract.elements.buttons[0]);
-// // 		console.log('IMAGES -->', Object.keys(extract.elements.images[0].styles).length);
-// // 	console.log('IMAGE[0].border -->', extract.elements.images[0].styles);
-//
-// 	const totals = {
-// 		'links'   : extract.elements.links.length,
-// 		'buttons' : extract.elements.buttons.length,
-// 		'images'  : extract.elements.images.length
-// 	};
-//
-//
-// 	let response = null;
-// 	try {
-// 		response = await queryPlayground(playgroundID, projectName());
-//
-// 	} catch (e) {
-// 		console.log('%s Error querying server! %s', chalk.red.bold('ERROR'), e);
-// 		process.exit(1);
-// 	}
-//
-// 	const playground = { ...response.playground,
-// 		id  : response.playground.id << 0,
-// 		new : response.playground.is_new
-// 	};
-//
-// 	console.log('%s Sending %s component(s)…', chalk.cyan.bold('INFO'), chalk.magenta.bold(Object.keys(totals).map((key)=> (totals[key])).reduce((acc, val)=> (acc + val))));
-// 	if (playground.new) {
-// 		response = await sendComponents(extract);
-// 	}
-//
-// 	await browser.close();
-// 	return ({ extract, totals, playground });
-// 	return (objs);
 }
 
 
--- src/utils.js
+++ src/utils.js
@@ -26,115 +26,49 @@ export async function captureScreenImage(page, encoding='base64') {
 
 export async function extractElements(page) {
 	const elements = {
-		'links'   : await Promise.all((await page.$$('a')).map(async(node)=> {
-
-// 			if (await node.boundingBox()) {
-// 				await node.hover();
-// 			}
-
-			let bounds = await node.boundingBox();
-			if (bounds) {
-				Object.keys(bounds).forEach((key)=> {
-					bounds[key] = (bounds[key] << 0);
-				});
-			}
-
-			const children = await node.$$eval('*', (els)=> els.map(({ outerHTML })=> (outerHTML.replace(/"/g, '\\"'))));
-			const attribs = await page.evaluate((el)=> {
-				const styles = elementStyles(el);
-
-				return ({
-					title  : el.id,
-					html   : el.outerHTML.replace(/"/g, '\\"'),
-					styles : styles,
-					elements : [],
-					meta : {
-						border : styles['border'],
-						color  : elementColor(styles),
-						font   : elementFont(styles),
-						text   : (/^<.+>$/.test(el.innerHTML)) ? '' : el.innerHTML,
-						href   : el.getAttribute('href')
-					}
-				});
-			}, node);
-
-			return ({...attribs, children, bounds,
-				meta : { ...attribs.meta,
-					text : (attribs.meta.text.length === 0 && children.length > 0) ? (await node.$$eval('*', (els)=> els.map(({ innerHTML })=> (innerHTML)))).filter((innerHTML)=> (innerHTML.length > 0 && !/^<.+>$/.test(innerHTML))).pop() : attribs.meta.text
-				},
-				box  : await node.boxModel()
-			});
-		})),
-
-		'buttons' : await Promise.all((await page.$$('button, input[type="button"], input[type="submit"]')).map(async(node)=> {
-
-			let bounds = await node.boundingBox();
-			if (bounds) {
-				Object.keys(bounds).forEach((key)=> {
-					bounds[key] = (bounds[key] << 0);
-				});
-			}
-
-			const children = await node.$$eval('*', (els)=> els.map(({ outerHTML })=> (outerHTML.replace(/"/g, '\\"'))));
-			const attribs = await page.evaluate((el)=> {
-				const styles = elementStyles(el);
-
-				return ({
-					title  : el.id,
-					html   : el.outerHTML.replace(/"/g, '\\"'),
-					styles : styles,
-					elements : [],
-					meta : {
-						border : styles['border'],
-						color  : elementColor(styles),
-						font   : elementFont(styles),
-						text   : (el.value.length === 0) ? (/^<.+>$/.test(el.innerHTML)) ? '' : el.innerHTML : el.value
-					}
-				});
-			}, node);
-
-			return ({...attribs, children, bounds,
-				meta : { ...attribs.meta,
-					text : (attribs.meta.text.length === 0 && children.length > 0) ? (await node.$$eval('*', (els)=> els.map(({ innerHTML })=> (innerHTML)))).filter((innerHTML)=> (innerHTML.length > 0 && !/^<.+>$/.test(innerHTML))).pop() : attribs.meta.text
-				},
-				box  : await node.boxModel()
-			});
-		})),
-
-		'images'  : await Promise.all((await page.$$('img')).map(async(node)=> {
-
-			let bounds = await node.boundingBox();
-			if (bounds) {
-				Object.keys(bounds).forEach((key)=> {
-					bounds[key] = (bounds[key] << 0);
-				});
-			}
-
-			const children = await node.$$eval('*', (els)=> els.map(({ outerHTML })=> (outerHTML.replace(/"/g, '\\"'))));
-			const attribs = await page.evaluate((el)=> {
-				const styles = elementStyles(el);
-
-				return ({
-					title  : el.id,
-					html   : el.outerHTML.replace(/"/g, '\\"'),
-					styles : styles,
-					elements : [],
-					meta : {
-						border : styles['border'],
-						color  : elementColor(styles),
-						font   : elementFont(styles),
-						text   : el.alt,
-						data   : imageData(el, elementBounds(el, styles).size),
-						url    : el.src
-					}
-				});
-			}, node);
-
-			return ({...attribs, children, bounds,
-				box : await node.boxModel()
-			});
-		}))
+		'links'   : await Promise.all((await page.$$('a', (nodes)=> (nodes))).map(async(node)=> (await processNode(page, node)))),
+		'buttons' : await Promise.all((await page.$$('button, input[type="button"], input[type="submit"]', (nodes)=> (nodes))).map(async(node)=> (await processNode(page, node)))),
+		'images'  : await Promise.all((await page.$$('img', (nodes)=> (nodes))).map(async(node)=> (await processNode(page, node))))
 	};
 
 	return (elements);
 }
+
+
+const processNode = async(page, node)=> {
+	let bounds = await node.boundingBox();
+	if (bounds) {
+		Object.keys(bounds).forEach((key)=> {
+			bounds[key] = (bounds[key] << 0);
+		});
+	}
+
+	const children = await Promise.all((await node.$$('*', (nodes)=> (nodes))).map(async(node)=> (await processNode(page, node))));
+	const attribs = await page.evaluate((el)=> {
+		const styles = elementStyles(el);
+
+		return ({
+			title   : (el.hasAttribute('alt') && el.alt.length > 0) ? el.alt : (el.hasAttribute('value') && el.value.length > 0) ? el.value : el.textContent,
+			html    : el.outerHTML.replace(/"/g, '\\"'),
+			styles  : styles,
+			classes : (el.className.length > 0) ? el.className : '',
+			meta    : {
+				border : styles['border'],
+				color  : elementColor(styles),
+				font   : elementFont(styles),
+				text   : (el.text || ''),
+				href   : (el.hasAttribute('href')) ? el.getAttribute('href') : null,
+				data   : (el.hasAttribute('src')) ? imageData(el, elementBounds(el, styles).size) : null,
+				url    : (el.hasAttribute('src')) ? el.getAttribute('src') : null,
+			}
+		});
+	}, node);
+
+	return ({...attribs, children, bounds,
+		title : ((attribs.title.length === 0) ? (attribs.meta.text.length === 0 && children.length > 0) ? (await node.$$eval('*', (els)=> els.map(({ innerHTML })=> (innerHTML)))).filter((innerHTML)=> (innerHTML.length > 0 && !/^<.+>$/.test(innerHTML))).pop() : attribs.meta.text : attribs.title),
+		meta  : { ...attribs.meta,
+			text : ((attribs.meta.text.length === 0 && children.length > 0) ? (await node.$$eval('*', (els)=> els.map(({ innerHTML })=> (innerHTML)))).filter((innerHTML)=> (innerHTML.length > 0 && !/^<.+>$/.test(innerHTML))).pop() : attribs.title)
+		},
+		box  : await node.boxModel()
+	});
+};
